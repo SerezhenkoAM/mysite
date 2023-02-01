a8c57605b6dd00e942674e6ec21d79fffde12072
SerezhenkoAM <123317970+SerezhenkoAM@users.noreply.github.com>
2023-02-01 22:11:27 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  publicPath: '/mysite/',
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mysite/'
+    : '/'
+}
